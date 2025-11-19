1e6074c93f11fff12588b43d4dcfae0b59f11e71
3ngery <omarangery2@gmail.com>
2025-11-19 18:59:38 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# simple python calculator
+simple command calc , created with python
+
+## Features
+1- ADD
+2- SUBTRACT
+3- MULTIPLY
+4- DIVIDE
+
+## HOW TO RUN 
+1\ clone the repository :
+    git clone <respository>
+2\ Run the project :
+    python main.py
+
---
+++ main.py
@@ -0,0 +1,32 @@
+#
+from calculator import add , subtract , multiply , divide
+
+def main():
+    print("==== angery's simple calculator ====")
+    print(" choose missod :")
+    print("1- ADD")
+    print("2- SUBTRACT")
+    print("3- MULTIPLY")
+    print("4- DIVIDE")
+
+    choice = input("enter choice ( 1 | 2 | 3 | 4 )")
+
+    if choice not in ["1","2","3","4"]:
+        print("Bad choice")
+        return
+    num1 = float(input("first input: ")).strip()
+    num2 = float(input("second input: ")).strip()
+
+    if choice == "1" :
+        result = add(num1,num2)
+    elif choice == "2":
+        result = subtract(num1,num2)
+    elif choice == "3":
+        result = multiply(num1,num2)
+    elif choice == "4":
+        result = divide(num1,num2)
+
+    print(f"Results : {result}")
+
+if __name__ == "__main__" :
+    main()
